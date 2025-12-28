3cff3c2e86f4735d1acd4dfe24e0dfe125010aac
Huy1188 <nghuy2052005@gmail.com>
2025-12-28 23:23:53 +0700

    Fix loi trang11

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,18 +1,13 @@
 import CategoryPage from "@/app/components/Pages/Category/index";
 import { notFound } from "next/navigation";
 
-type Props = {
-  params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
+export default async function CategorySlugPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
 
-// export default function CategorySlugPage({ params }: Props) {
-//   const { slug } = params;
-//   if (!slug) notFound();
-
-//   return <CategoryPage slug={slug} />;
-// }
-
-export default function CategorySlugPage({ params }: { params: { slug: string } }) {
-  return <div style={{ padding: 40 }}>CATEGORY OK: {params.slug}</div>;
+  if (!slug) notFound();
+  return <CategoryPage slug={slug} />;
 }
